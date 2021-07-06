87d5b5dab8284642a1d4bab2cf93817f03a0f6fc
scidroid <scidroid@scidroid.me>
2021-07-06 16:17:18 -0500

    feat: :lipstick: some changes

--- static/commerce.js
+++ static/commerce.js
@@ -1,3 +1,7 @@
 import Commerce from '@chec/commerce.js';
 
 const commerce = new Commerce('{public_api_key}');
+
+export const fetchProducts = _ => {
+    commerce.products.list().then((product) => return product);
+};
