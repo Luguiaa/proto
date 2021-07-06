34d1e01d542d5b133d5c0b3104120941ddb385a3
scidroid <scidroid@scidroid.me>
2021-07-06 07:33:13 -0500

    fix: :bug: Fix mayor bugs with main page

--- js/form.js
+++ js/form.js
@@ -1,34 +1,31 @@
-const func = async _ => {
-  await fetch("/api/answers.json")
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+let index = 0;
+const render = (data) => {
+  document.getElementById("main").innerHTML = `
+        <h2>${data[index].question}</h2>
+        <ul>
+          <li>
+            <a onclick="form(1)">${data[index].firstImage}</a>
+            <p>${data[index].firstAlt}</p>
+          </li>
+          <li>
+            <a onclick="form(2)">${data[index].secondImage}</a>
+            <p>${data[index].secondAlt}</p>
+          </li>
+        </ul>
+  `;
 };
 
-const formItems = func();
-
-let index = 0;
-
 const form = (option) => {
-  console.log("hola");
-  if (option === 2 && index === 1) {
-    window.open("/store");
-  }
-  document.querySelector("main").innerHTML = `
-  <h2>${formItems[index].question}</h2>
-  <ul>
-    <li>
-      <a onclick="form(1)">${formItems[index].firstImage}</a>
-      <p>${formItems[index].firstAlt}</p>
-    </li>
-    <li>
-      <a onclick="form(2)">${formItems[index].secondImage}</a>
-      <p>${formItems[index].secondAlt}</p>
-    </li>
-  </ul>
-  `;
-  index++;
+  fetch("/api/answers.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (option === 2 && index === 1) {
+        window.open("/store");
+      }
+      render(data);
+      index++;
+    });
 };
 
 form(0);
