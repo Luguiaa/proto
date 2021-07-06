c7adcfe31a1695224e3a001dd56f0ae84ad29445
scidroid <scidroid@scidroid.me>
2021-07-06 14:52:17 -0500

    feat: :art: add a lot of things

---
+++ static/card.js
@@ -0,0 +1,21 @@
+const card = (container, position) => {
+  fetch("/products.json")
+    .then((res) => res.json())
+    .then((products) => {
+      document.querySelector(container).insertAdjacentHTML(
+        "beforeend",
+        `
+    <div class="card" style="width: 18rem; margin: 15px;">
+        <img src="${products[position].img}" class="card-img-top" alt="${products[position].alt}">
+        <div class="card-body">
+            <h5 class="card-title">${products[position].title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${products[position].subtitle}</h6>
+            <p class="card-text">${products[position].description}</p>
+            <a href="${products[position].url}" class="card-link">Comprar</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+    `
+      );
+    });
+};
--- static/preferences-quiz.js
+++ static/preferences-quiz.js
@@ -1,75 +1,117 @@
 let stage = 0,
-preferences = {
+  preferences = {
     assistedShopping: false,
     artProducts: false,
     cholesterol: false,
     interestedInCoffee: false,
-},
-title = document.querySelector("#title"),
-info = document.querySelector("#info"),
-yesButton = document.querySelector("#yes"),
-noButton = document.querySelector("#no");
+  },
+  container = document.querySelector("#anchors"),
+  title = document.querySelector("#title"),
+  info = document.querySelector("#info"),
+  yesButton = document.querySelector("#yes"),
+  noButton = document.querySelector("#no");
 
-const err = _ => {
-    document.querySelector("#anchors").style.display = "none";
-    title.innerHTML = "Ocurrió un error en el servidor."
-    info.innerHTML = "Intenta refrescando la página."
-}
+const err = (_) => {
+  document.querySelector("#anchors").style.display = "none";
+  title.innerHTML = "Ocurrió un error en el servidor.";
+  info.innerHTML = "Intenta refrescando la página.";
+};
 
-const processData = async _ => {
-    fetch("/api/receiveInterestData", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(preferences)
+const processData = async (_) => {
+  fetch("/api/receiveInterestData", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(preferences),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success !== true) {
+        err();
+        return;
+      }
+      console.log("success");
     })
-    .then(res => res.json())
-    .then(res => {
-        if(res.success !== true) {
-            err();
-            return;
+    .catch((_) => err);
+};
+
+const quiz = (_) => {
+  console.log(preferences);
+  switch (stage) {
+    case 0:
+      title.innerHTML = "¿Deseas habilitar la compra asistida?";
+      info.innerHTML =
+        "La compra asistida utiliza Inteligencia Artificial para recomendarte productos";
+
+      noButton.onclick = (_) => {
+        window.location.href = "/store";
+      };
+      yesButton.onclick = (_) => {
+        preferences.assistedShopping = true;
+        stage++;
+        quiz();
+      };
+      break;
+    case 1:
+      title.innerHTML =
+        "¿Te interesan los productos elaborados artesanalmente?";
+      info.innerHTML =
+        "Estos datos serán utilizados para recomendarte productos nuevos";
+
+      noButton.onclick = (_) => {
+        stage++;
+        quiz();
+      };
+      yesButton.onclick = (_) => {
+        preferences.artProducts = true;
+        stage++;
+        quiz();
+      };
+      break;
+    case 2:
+      title.innerHTML = "¿Tienes problemas de colesterol o presión alta?";
+      info.style.color = "red";
+      info.innerHTML = "Te recomendaremos productos que te sirvan.";
+
+      noButton.onclick = (_) => {
+        stage++;
+        quiz();
+      };
+      yesButton.onclick = (_) => {
+        preferences.cholesterol = true;
+        stage++;
+        quiz();
+      };
+      break;
+    case 3:
+      title.innerHTML = "¿Te gusta el café?";
+      info.style.color = "#222222";
+      info.innerHTML = "Queremos conocer tus gustos";
+
+      noButton.onclick = (_) => {
+        {
+          stage++;
+          quiz();
+          processData();
         }
-        console.log("success");
-    })
-    .catch(_ => err);
-}
-
-const quiz = _ => {
-    console.log(preferences)
-    switch(stage) {
-        case 0:
-            title.innerHTML = "¿Deseas habilitar la compra asistida?";
-            info.innerHTML = "La compra asistida utiliza Inteligencia Artificial para recomendarte productos";
-
-            noButton.onclick = _ => { window.location.href = "/store" }
-            yesButton.onclick = _ => { preferences.assistedShopping = true; stage++; quiz(); }
-        break;
-        case 1:
-            title.innerHTML = "¿Te interesan los productos elaborados artesanalmente?";
-            info.innerHTML = "Estos datos serán utilizados para recomendarte productos nuevos";
-
-            noButton.onclick = _ => { stage++; quiz(); }
-            yesButton.onclick = _ => { preferences.artProducts = true; stage++; quiz(); }
-        break;
-        case 2:
-            title.innerHTML = "¿Tienes problemas de colesterol o presión alta?";
-            info.style.color = "red";
-            info.innerHTML = "Este paso es importante para no recomendarte productos dañinos.";
-
-            noButton.onclick = _ => { stage++; quiz(); }
-            yesButton.onclick = _ => { preferences.cholesterol = true; stage++; quiz(); }
-        break;
-        case 3:
-            title.innerHTML = "¿Te gusta el café?";
-            info.style.color = "#222222";
-            info.innerHTML = "";
-
-            noButton.onclick = processData;
-            yesButton.onclick = processData;
-        break;
-    }
-}
+      };
+      yesButton.onclick = (_) => {
+        preferences.cholesterol = true;
+        stage++;
+        quiz();
+        processData();
+      };
+      break;
+    case 4:
+      title.innerHTML = "Te recomendamos los siguientes productos";
+      info.innerHTML = "Para continuar tendrás que iniciar sesión";
+      container.innerHTML = "";
+      card("#anchors", 0);
+      card("#anchors", 1);
+      break;
+  }
+};
 
 window.onload = quiz();
